7d023509fbf8e99e2bf61832f462cf98e3f9a654
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-02 17:44:56 +0530

    Loading functionality is added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  async function fetchMoviesHandler() {
+    setIsLoading(true)
+    const res = await fetch('https://swapi.dev/api/films')
+    const data = await res.json()
+      const transformedData = data.results.map((movieData) => {
+        return{
+          id: movieData.episode_id,
+          titile: movieData.titile,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date
+        }
+      })
+      setMovies(transformedData);
+      setIsLoading(false);
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...!</p>}
       </section>
     </React.Fragment>
   );
