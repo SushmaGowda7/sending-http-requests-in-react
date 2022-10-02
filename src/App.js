46b0cb213d2449c52565820aecb1a079ae87d941
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-02 21:44:54 +0530

    Fetch movie on page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -6,14 +6,14 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [stop, setStop] = useState(null);
-  
-  async function fetchMoviesHandler() {
+  // const [stop, setStop] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async() => {
     setIsLoading(true)
     setError(null)
-    setStop(null)
+    // setStop(null)
     try {
-      const res = await fetch('https://swapi.dev/api/film')
+      const res = await fetch('https://swapi.dev/api/films')
       if(!res.ok){
         throw new Error('Something went wrong ....Retrying')
       }
@@ -32,21 +32,23 @@ function App() {
       
     } catch (error) {
       setError(error.message);
-      
-      const interval = setInterval(async() => {
-        await fetch("https://swapi.py4e.com/api/films/");
-      }, 5000);
-
-      <button onClick={stopRetryingHandler}>Cancel</button>
-      setStop(interval);
+      // const interval = setInterval(async() => {
+      //   await fetch("https://swapi.py4e.com/api/films/");
+      // }, 5000);
+      // <button onClick={stopRetryingHandler}>Cancel</button>
+      // setStop(interval);
     }
     setIsLoading(false);
 
-  };
+  },[]);
 
-  const stopRetryingHandler = () => {
-    clearInterval(stop);
-  };
+  useEffect(() => {
+    fetchMoviesHandler() 
+  }, [fetchMoviesHandler]);
+  
+  // const stopRetryingHandler = () => {
+  //   clearInterval(stop);
+  // };
     
   let content = <p>Found no movies!</p>;
 
