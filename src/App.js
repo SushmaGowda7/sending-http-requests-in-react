749af66e7c5b6d3446d5a54a2bb3ec7d8b5c11f3
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2022-10-07 11:40:58 +0530

    sorted and deleting the movies using delete method

--- src/App.js
+++ src/App.js
@@ -12,21 +12,22 @@ function App() {
     setIsLoading(true)
     setError(null)
     try {
-      const res = await fetch('https://swapi.dev/api/films')
+      const res = await fetch('https://react-http-requests-3c6c0-default-rtdb.firebaseio.com/movies.json')
       if(!res.ok){
         throw new Error('Something went wrong ....Retrying')
       }
-    const data = await res.json()
+    const data = await res.json();
 
-      const transformedData = data.results.map((movieData) => {
-        return{
-          id: movieData.episode_id,
-          titile: movieData.titile,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
+    const addeddMovies = [];
+    for (const key in data) {
+      addeddMovies.push({
+        id: key,
+        titile: data[key].titile,
+        openingText: data[key].openingText,
+        releaseDate: data[key].releaseDate
       })
-      setMovies(transformedData);
+    } 
+      setMovies(addeddMovies);
       setIsLoading(false);
       
     } catch (error) {
@@ -40,14 +41,28 @@ function App() {
     fetchMoviesHandler() 
   }, [fetchMoviesHandler]);
 
-  const addMovieHandler = (movie) => {
-    console.log(movie);
-  }
-
+  const addMovieHandler = async(movie) => {
+    const res = await fetch('https://react-http-requests-3c6c0-default-rtdb.firebaseio.com/movies.json', 
+    {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {'Content-type': 'application/json'}
+  })
+  const data = await res.json();
+  console.log(data);
+}
+const deleteMovieHandler = async(id) => {
+  const res = await fetch(`https://react-http-requests-3c6c0-default-rtdb.firebaseio.com/movies/${id}.json`, 
+  {
+    method: 'DELETE',
+})
+const data = await res.json();
+console.log(data);
+}
   let content = <p>Found no movies!</p>;
 
   if(movies.length > 0){
-    content = <MoviesList movies={movies} />
+    content = <MoviesList onDelete={deleteMovieHandler} movies={movies} />
   }
 
   if(error){
